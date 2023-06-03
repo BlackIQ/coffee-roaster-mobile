dbe842bce3b4885acef2215e3929f130db4eac0e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-06-04 02:06:53 +0330

    Auth done

--- lib/src/screens/auth/auth.screen.dart
+++ lib/src/screens/auth/auth.screen.dart
@@ -1,15 +1,75 @@
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  void hi() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => AlertDialog(
+        title: Text('Username is ${_username.text}'),
+        content: const Text('Do you confirm your username?'),
+        actions: [
+          TextButton(onPressed: () => {}, child: const Text("Ok")),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            style: TextButton.styleFrom(foregroundColor: Colors.red),
+            child: const Text("No"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Auth"),
+        title: const Text("Login to your account"),
       ),
-      body: const Center(child: Text("Auth")),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _username,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Username"),
+                hintText: "Enter your username",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+                controller: _password,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Password"),
+                  hintText: "Enter your password",
+                ),
+                obscureText: true),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: hi,
+                child: const Text("Login"),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
