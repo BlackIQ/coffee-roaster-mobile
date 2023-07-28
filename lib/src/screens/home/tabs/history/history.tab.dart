c839fa9a329682a55c3f9b2fe568dd0cfbd9799f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-28 14:07:37 +0330

    Sample of history

---
+++ lib/src/screens/home/tabs/history/history.tab.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+import 'package:roaster/src/services/state/state.service.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HistoryTab extends StatefulWidget {
+  const HistoryTab({super.key});
+
+  @override
+  State<HistoryTab> createState() => _HistoryTabState();
+}
+
+List histories = [
+  {
+    'id': 1,
+    'name': 'amir',
+  },
+  {
+    'id': 2,
+    'name': 'ali',
+  },
+  {
+    'id': 3,
+    'name': 'nilo',
+  }
+];
+
+class _HistoryTabState extends State<HistoryTab> {
+  Future<void> _showSnackBar(BuildContext context, String message) async {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 1),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = AppLocalizations.of(context);
+
+    bool isGuest = Provider.of<AppState>(context, listen: true).getGuest;
+
+    return isGuest
+        ? Center(
+            child: Text(
+              lang!.bottom_navigator_history,
+            ),
+          )
+        : ListView.separated(
+            itemCount: histories.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const Divider();
+            },
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(histories[index]['name']),
+                onTap: () {},
+              );
+            },
+          );
+  }
+}
