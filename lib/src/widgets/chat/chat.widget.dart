f2c367da2ff463bb5f081e40c082c78651459150
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-28 21:21:31 +0330

    Create some new widgets

---
+++ lib/src/widgets/chat/chat.widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ChatContainer extends StatelessWidget {
+  final String text;
+  final Color fgcolor;
+  final Color bgcolor;
+  final String side;
+
+  const ChatContainer({
+    Key? key,
+    required this.text,
+    required this.fgcolor,
+    required this.bgcolor,
+    required this.side,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: bgcolor,
+        borderRadius: BorderRadius.only(
+          topRight: const Radius.circular(13),
+          topLeft: const Radius.circular(13),
+          bottomLeft: Radius.circular(side == "right" ? 13 : 0),
+          bottomRight: Radius.circular(side == "left" ? 13 : 0),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          text,
+          style: TextStyle(color: fgcolor),
+        ),
+      ),
+    );
+  }
+}
